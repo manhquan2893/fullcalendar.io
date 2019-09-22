ab44dac2605676af63aff4385baf1f0c0cbd18c8
Admin <manhquan2893@gmail.com>
2019-09-22 20:38:03 +0700

    seed

--- config/database.php
+++ config/database.php
@@ -6,7 +6,7 @@ $url = parse_url(getenv('DATABASE_URL'));
 $host = $url['host'] ?? null;
 $username = $url['user'] ?? null;
 $password = $url['pass'] ?? null;
-$database = substr($url['path'], 1) ?? null;
+$database = substr($url['path'], 1) ;
 
 return [
 
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('users')->insert([
+            'name' => 'manhquan',
+            'email' => 'manhquan2893@gmail.com',
+            'password' => '1',
+        ]);
+    }
+}
